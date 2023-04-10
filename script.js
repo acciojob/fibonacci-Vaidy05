4f3a9fbf26c0b8336aba0906a27a84f17ee7b1ec
Vaidy05 <92988611+Vaidy05@users.noreply.github.com>
2023-04-10 13:45:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function fibonacci(num) {
 // your code here
+	let n1=0,n2=1,next;
+	for(int i=1;i<=num;i++){
+		next = n1 + n2;
+        n1 = n2;
+        n2 = next;
+	}
+	return n1;
 }
 
 module.exports = fibonacci;
